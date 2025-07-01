a446864f2ebcffb22276ee60987e393ffb895b8d
paras1234-eng <paras00333@gmail.com>
2025-07-01 08:51:37 +0100

    Delete final_output_review.sql

--- final_output_review.sql
+++
@@ -1,8 +0,0 @@
--- View clean release records
-SELECT * FROM release_instructions ORDER BY release_date DESC;
-
--- View exceptions that need investigation
-SELECT * FROM exceptions_report ORDER BY issue, client_id;
-
--- Optional: Count exceptions by type
-SELECT issue, COUNT(*) AS count FROM exceptions_report GROUP BY issue;
